0e48a37684945e36454dc0e75f58ba619d243af8
Harleen Kaur <harleenkaur1343@gmail.com>
2025-12-13 12:49:23 +0530

    Update chat.js for delete chat

--- major_project/backend/routes/chat.js
+++ major_project/backend/routes/chat.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getChat, getChats} = require('../controllers/chatController');
+const { getChat, getChats, deleteChat} = require('../controllers/chatController');
 const requireAuth = require('../middleware/requireAuth');
 
 router.use(requireAuth);
-router.route("/").post(getChat).get(getChats);
+router.route("/").post(getChat).get(getChats).delete(deleteChat);
 
 module.exports = router;
